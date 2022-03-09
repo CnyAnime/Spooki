ad6ec955ef6b6c673730415fb4c5d9dfb1d90be5
jotte <71946106+jottew@users.noreply.github.com>
2022-03-09 19:40:31 +0100

    Add download command

--- spooki/cogs/utility/_base.py
+++ spooki/cogs/utility/_base.py
@@ -1,4 +1,9 @@
 from spooki.utils.subclasses import Cog
+from bs4 import BeautifulSoup
 
 class BaseUtilityCog(Cog):
-    pass
+    async def parse_roblox_audio(self, url: str):
+        content = await (await self.bot.session.get(url)).read()
+        bs = BeautifulSoup(content, "html.parser")
+        button = bs.find("div", class_="MediaPlayerIcon icon-play")
+        return button.get("data-mediathumb-url")
