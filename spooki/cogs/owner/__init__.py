725e91de81423a340cbb37ca8fddd1104938f06a
Ender2K89 <ender2k89@gmail.com>
2022-04-05 14:53:44 +0200

    actually no

--- spooki/cogs/owner/__init__.py
+++ spooki/cogs/owner/__init__.py
@@ -1,15 +1,11 @@
-LOCAL = False # set this to True if a private.py file exists in this directory
-
 from spooki.bot import Spooki
 from ._base import BaseOwnerCog
 from .blacklist import BlacklistMixin
 from .extensions import ExtensionMixin
-
-if LOCAL:
-    from .private import PrivateMixin
+# from .private import PrivateMixin
 
 
-class OwnerCog(BaseOwnerCog, BlacklistMixin, ExtensionMixin, PrivateMixin or None, name="Owner"):
+class OwnerCog(BaseOwnerCog, BlacklistMixin, ExtensionMixin, name="Owner"):
     pass
 
 
